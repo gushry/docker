f71a75a9875f89b63d2145ef6feb4c6042fba37b
gushry <gushry@gmail.com>
2015-07-23 08:14:24 +0000

    change entrypoint

--- runtime-java-with-newrelic/scripts/newrelic.sh
+++ runtime-java-with-newrelic/scripts/newrelic.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -x
 
-if [ -z ${NEWRELIC_LICENSE_KEY} ]; then
-  exit 0
-fi
-nrsysmond-config --set license_key=$NEWRELIC_LICENSE_KEY
+if [ -n ${NEWRELIC_LICENSE_KEY} ]; then
+  nrsysmond-config --set license_key=$NEWRELIC_LICENSE_KEY
 
-/etc/init.d/newrelic-sysmond stop
-/etc/init.d/newrelic-sysmond start
+  /etc/init.d/newrelic-sysmond stop
+  /etc/init.d/newrelic-sysmond start
+fi
+
+exec "$@"
